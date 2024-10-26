66337a9743793436132c9adc9bfbc6fa03899e93
nick123456789010 <137611297+nick123456789010@users.noreply.github.com>
2024-10-26 22:53:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const routes = {
   "te.nhrnick.us.kg":"https://telegram.org",
   "dockerhub.nhrnick.us.kg":"https://hub.docker.com",
   "github.nhrnick.us.kg":"https://github.com/",
-  "planel4server00":"planel4.seve00.com"
+  "planel4server00.nhrnick.us.kg":"planel4.seve00.com"
     
   // staging
   "docker-staging.nhrnick.us.kg": dockerHub,
